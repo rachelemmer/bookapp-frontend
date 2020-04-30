cf37e01daacc060b2cacb5cb92a242cdcb9b4e51
rachelemmer <rachelaemmer@gmail.com>
2020-04-30 11:20:23 -0600

    add delete functionality

--- src/BookCard.js
+++ src/BookCard.js
@@ -1,41 +1,48 @@
 import React from 'react'
 
-const BookCard = (props) => {
+function BookCard(props) {
     
     const {book, displayFavorite, removeFromFavorites} = props
 
-    handleClick = () => {
+    function handleClick() {
         if (displayFavorite) {
+            displayFavorite(book)
             postBook(book) }
         else {
             removeFromFavorites(book)
+            deleteBook(book)
         }
-    }
-        
     
-    const postBook = () => {   
-        fetch("http://localhost:3000/books", {
-                method: "POST",
-                headers: { 
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${localStorage.getItem("token")}`
-                },
-                body: JSON.stringify({
-                    title: book.volumeInfo.title,
-                    author: book.volumeInfo.author,
-                    image: book.volumeInfo.imageLinks ? props.book.volumeInfo.imageLinks.thumbnail : "",
-                    api_id: book.id
-                })
-            }).then(response => response.json())
-            .then(result => console.log(result))
-            .catch(error => console.error(error))
+        function postBook() {   
+            fetch("http://localhost:3000/books", {
+                    method: "POST",
+                    headers: { 
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${localStorage.getItem("token")}`
+                    },
+                    body: JSON.stringify({
+                        title: book.volumeInfo.title,
+                        author: book.volumeInfo.author,
+                        image: book.volumeInfo.imageLinks ? props.book.volumeInfo.imageLinks.thumbnail : "",
+                        api_id: book.id
+                    })
+                }).then(response => response.json())
+                .then(result => console.log(result))
+                .catch(error => console.error(error))
+        }
+
+        function deleteBook() {
+            fetch(`http://localhost:3000/books/${book.id}`, {
+                method: 'DELETE'
+            })
         }
     }
     
     return (
         <div className="card" onClick={handleClick}>
-            <img src={props.image} alt=""/>
+            <img src={props.image} alt={props.title}/>
         </div>
     )
+}
 
 export default BookCard;
